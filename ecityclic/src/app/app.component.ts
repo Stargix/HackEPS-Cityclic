e286e9b93ccda792a8c9a006273343a04255c7e6
Eduardo7125 <eduardocristea55@gmail.com>
2024-11-24 00:06:01 +0100

    Footer mas o menos acabado, se pueden cambiar cosas si se quiere(No definitivo)

--- ecityclic/src/app/app.component.ts
+++ ecityclic/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "./title/title.component";
 import { SearchComponent } from "./buscador/buscador.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [TitleComponent, SearchComponent]
+  imports: [TitleComponent, SearchComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'ecityclic';
--- ecityclic/src/app/footer/footer.component.ts
+++ ecityclic/src/app/footer/footer.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css'],
+  imports: []
+
 })
 export class FooterComponent {
 
